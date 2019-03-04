a3470596ef1906f146bd8dcff15eb2f0005b9fb7
sidemastergm <jersonmartinezsm@gmail.com>
2019-03-03 20:56:29 -0600

    Creating new spaces as contact, portfolio and services

--- php/hd-menu.php
+++ php/hd-menu.php
@@ -1,18 +1,71 @@
 <div class="container-fluid first-container-fluid">
     <div class="container">
         <ul class="nav nav-pills">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">HOME</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">SERVICES</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">PORTFOLIO</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">CONTACT</a>
-            </li>
+
+            <?php
+                $filename = basename($_SERVER['PHP_SELF']);
+
+                if ($filename == "index.php"){
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./">HOME</a>
+                        </li>
+                    <?php
+                } else {
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./">HOME</a>
+                        </li>
+                    <?php
+                }
+
+                if ($filename == "services.php"){
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./services.php">SERVICES</a>
+                        </li>
+                    <?php
+                } else {
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./services.php">SERVICES</a>
+                        </li>
+                    <?php
+                }
+
+                if ($filename == "portfolio.php"){
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./portfolio.php">PORTFOLIO</a>
+                        </li>
+                    <?php
+                } else {
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./portfolio.php">PORTFOLIO</a>
+                        </li>
+                    <?php
+                }
+
+                if ($filename == "contact.php"){
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./contact.php">CONTACT</a>
+                        </li>
+                    <?php
+                } else {
+                    ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./contact.php">CONTACT</a>
+                        </li>
+                    <?php
+                }
+            ?>
+
+            
+            
+            
+            
         </ul>
     </div>
 </div>
---
+++ services.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include ("php/head.php"); ?>
+	</head>
+
+	<body>
+		<?php
+			include ("php/header.php");
+
+			include ("php/content.php");
+			
+			include ("php/footer.php");
+		?>
+	</body>
+</html>
